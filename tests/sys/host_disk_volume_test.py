9c983cd6e9d42cb4f262e709542c768b37aec52a
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-10-15 10:44:49 +0100

    Added disk_volume.py top-level script

---
+++ tests/sys/host_disk_volume_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+Created on 15 Oct 2020
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+import json
+
+from scs_core.data.json import JSONify
+from scs_core.sys.disk_volume import DiskVolume
+
+from scs_host.sys.host import Host
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+mounted_on = '/srv/SCS_logging'
+print(mounted_on)
+
+volume = Host.disk_volume(mounted_on)
+print(volume)
+
+print("-")
+
+mounted_on = '/'
+print(mounted_on)
+
+volume = Host.disk_volume(mounted_on)
+print(volume)
+
+usage = Host.disk_usage(mounted_on)
+print(usage)
+
+print("-")
+
+jstr = JSONify.dumps(volume.as_json())
+print(jstr)
+
+jdict = json.loads(jstr)
+volume = DiskVolume.construct_from_jdict(jdict)
+print(volume)
+
