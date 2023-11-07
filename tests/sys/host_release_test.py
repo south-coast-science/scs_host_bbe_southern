7da34f39ae32bc16b6cb9e456d765e2d8df17429
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-11-07 13:15:37 +0000

    Added is_acceptable_os_release() method to IoTNode

---
+++ tests/sys/host_release_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Created on 7 Nov 2023
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from scs_host.sys.host import Host
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+print("minimum: %s" % Host.MINIMUM_OS_RELEASE)
+
+print("acceptable: %s" % Host.is_acceptable_os_release())
